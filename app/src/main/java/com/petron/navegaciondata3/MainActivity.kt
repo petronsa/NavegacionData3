3f0c756c16519a4437c10b7069cc90b28d5e14e6
Petron <androidtorrejoncillo1@gmail.com>
2021-08-06 21:21:17 +0200

    Añadida varias clases y sus xml, el navegador de fragment tambien esta añadido

--- app/src/main/java/com/petron/navegaciondata3/MainActivity.kt
+++ app/src/main/java/com/petron/navegaciondata3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.petron.navegaciondata3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.petron.navegaciondata3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // Necesario layout en el xml o no funciona
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        // controlador de navegación para agregar un botón atras Arriba a su aplicación
+        val navController = this.findNavController(R.id.myNavHostFragment)
+    }
+
+
+    //método para llamar navigateUp()al controlador de navegación
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
