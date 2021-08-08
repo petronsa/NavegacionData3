6bff725417f3e4423f58dff3e426a9052324f46a
Petron <androidtorrejoncillo1@gmail.com>
2021-08-08 14:03:26 +0200

    Añadido drawer_layout y su layaout correspondiente, solo faltaria añadir botones al fragment uno y más fragment para navegar, para usar por ejemplo en una aplicación escaleras y cada modelo tuviera su fragment

--- app/src/main/java/com/petron/navegaciondata3/MainActivity.kt
+++ app/src/main/java/com/petron/navegaciondata3/MainActivity.kt
@@ -3,26 +3,37 @@ package com.petron.navegaciondata3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.petron.navegaciondata3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Variable cajon de navegacion (menu lateral)
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Necesario layout en el xml o no funciona
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        //Inicializamos el drawer_layout
+
+        drawerLayout = binding.drawerLayout
 
         // controlador de navegación para agregar un botón atras Arriba a su aplicación
         val navController = this.findNavController(R.id.myNavHostFragment)
 
         NavigationUI.setupActionBarWithNavController(this,navController)
+        //Para mostrar el panel de navegación (menu lateral)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        // Para mostrar el icono del drawer_layout
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
     }
 
 
     //método para llamar navigateUp()al controlador de navegación
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
